bc1639f695bc48551c127f8dbfa38ac035816895
LaZy1220 <4elove4iska@gmail.com>
2022-06-18 15:34:58 +0300

    add footer and header

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+export function Footer() {
+    return (
+        <footer className='footer'>
+            <div className='footer__line'></div>
+            <span className='footer__text'> ©{new Date().getFullYear()} Copyright Text</span>
+            <a className='footer__link' href='https://github.com/LaZy1220' target='blank'>Git</a>
+        </footer>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+export function Header() {
+    return (
+        <header className='header'>
+            <span className='header__logo'>LaZy Movies</span>
+            <a className='header__link' href='https://github.com/LaZy1220' target='blank'>Git</a>
+        </header>
+    )
+}   
