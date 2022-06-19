33716c91f25c83023e770541965522b2c8198fb8
LaZy1220 <4elove4iska@gmail.com>
2022-06-19 15:21:51 +0300

    new preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,15 +1,7 @@
 export function Preloader() {
     return (
-        <div className="preloader-wrapper big active">
-            <div className="spinner-layer spinner-blue-only">
-                <div className="circle-clipper left">
-                    <div className="circle"></div>
-                </div><div className="gap-patch">
-                    <div className="circle"></div>
-                </div><div className="circle-clipper right">
-                    <div className="circle"></div>
-                </div>
-            </div>
-        </div>
+  <div class="progress">
+  <div class="indeterminate"></div>
+</div>
     )
 }
